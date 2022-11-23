29801fd5c91511708a42a26f11b99e8a53c03630
Scottacus64 <70824106+Scottacus64@users.noreply.github.com>
2022-11-22 22:45:05 -0600

    cMake Files Added

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/scottmiller/VSC/Dice
+# Build directory: /Users/scottmiller/VSC/Dice/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
